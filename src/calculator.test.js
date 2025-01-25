9d13a94bfd65fa73456c9f09e3609bb3d7161bc5
ScottGrieveWeb <scottgrieveweb@gmail.com>
2025-01-25 18:25:24 +0000

    Tests written for calculator assignment

---
+++ src/calculator.test.js
@@ -0,0 +1,18 @@
+import { calculator } from "./calculator";
+
+test('enter 5 and 2, returns 7', () => {
+  expect(calculator.add(5, 2)).toBe(7);
+});
+
+test('enter 5 and 2, returns 3', () => {
+    expect(calculator.subtract(5, 2)).toBe(3);
+  });
+
+test('enter 8 and 2, returns 4', () => {
+  expect(calculator.divide(8, 2)).toBe(4);
+});
+
+test('enter 8 and 2, returns 16', () => {
+    expect(calculator.divide(8, 2)).toBe(16);
+  });
+
